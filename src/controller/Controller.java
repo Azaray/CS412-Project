6f06f024b637cd541206d40410d043d14bfe09b5
Andrew Scott <drb13175@ds.strath.ac.uk>
2016-11-16 14:52:05 +0000

    Refactoring Controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -7,7 +7,6 @@ import java.util.Observer;
 
 import datastructures.SearchField;
 import datastructures.SearchQuery;
-import model.Indexer;
 import model.Model;
 import view.View;
 
@@ -20,8 +19,57 @@ public class Controller implements ActionListener {
 		System.out.println("Controller: Controller()");
 	}
 
+	public void addModel(Observable m) {
+		System.out.println("Controller: adding Model");
+		this.Model = (Model) m;
+	}
+
+	public void addView(Observer v) {
+		System.out.println("Controller: adding view");
+		this.View = (View) v;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		if (e.getActionCommand().equals("Refresh"))
+			this.Model.Refresh();
+		else if (e.getActionCommand().equals("Search"))
+			Search();
+		else if (e.getActionCommand().equals("Next"))
+			NextPage();
+		else if (e.getActionCommand().equals("Previous"))
+			PreviousPage();
+
+		System.out.println("Controller: actionPerformed() : The " + e.getActionCommand() + " button is clicked at "
+				+ new java.util.Date(e.getWhen()) + " with e.paramString " + e.paramString());
+	}
+
+	private void Search() {
+		SearchQuery searchQuery = new SearchQuery(this.View.getSearchField(), SearchField.DOCCONTENT, false);
+		this.Model.Search(searchQuery);
+	}
+
+	private void NextPage() {
+		if ((this.View.getPageNumber() + 1) * 10 < this.View.getSuggestionsList().size()) {
+			this.View.setPageNumber(this.View.getPageNumber() + 1);
+			this.View.fillSuggestions();
+		} else {
+			System.out.println("End reached");
+		}
+	}
+
+	private void PreviousPage() {
+		if ((this.View.getPageNumber()) > 0) {
+			this.View.setPageNumber(this.View.getPageNumber() - 1);
+			this.View.fillSuggestions();
+		} else {
+			System.out.println("End reached");
+		}
+	}
+
 	public static void main(String[] args) {
-		
+
 		// Create Model and Controller
 		Model Model = new Model();
 		Controller myController = new Controller();
@@ -37,43 +85,4 @@ public class Controller implements ActionListener {
 		myController.addModel(Model);
 		myController.addView(myView);
 	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		System.out.println("Controller: actionPerformed() : The " + e.getActionCommand() + " button is clicked at "
-				+ new java.util.Date(e.getWhen()) + " with e.paramString " + e.paramString());
-		
-		if (e.getActionCommand().equals("Refresh")) this.Model.Refresh();
-		else if(e.getActionCommand().equals("Search")) {
-			SearchQuery searchQuery = new SearchQuery(this.View.getSearchField(), SearchField.DOCCONTENT, false);
-			this.Model.Search(searchQuery);
-		} else if(e.getActionCommand().equals("Next")) {
-			if((this.View.getPageNumber()+1) * 10 < this.View.getSuggestionsList().size()) {
-				this.View.setPageNumber(this.View.getPageNumber()+1);
-				this.View.fillSuggestions();
-			} else {
-				System.out.println("End reached");
-			}
-
-		} else if(e.getActionCommand().equals("Previous")) {
-			if((this.View.getPageNumber()) > 0) {
-				this.View.setPageNumber(this.View.getPageNumber()-1);
-				this.View.fillSuggestions();
-			} else {
-				System.out.println("End reached");
-			}
-
-		}
-	}
-
-	public void addModel(Observable m) {
-		System.out.println("Controller: adding Model");
-		this.Model = (Model) m;
-	}
-
-	public void addView(Observer v) {
-		System.out.println("Controller: adding view");
-		this.View = (View) v;
-	}
-
 }
