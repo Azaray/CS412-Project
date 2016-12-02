b3bac0c3e6baeb34a830822f1703e0df2508f31d
zveket <kirilrupasov@gmail.com>
2016-12-02 12:18:21 +0000

    removed redundant code

--- src/view/View.java
+++ src/view/View.java
@@ -151,7 +151,6 @@ public class View extends JFrame implements Observer {
 
 	private void createAndShowGUI() {
 		//Create and set up the window.
-		//JFrame frame = new JFrame("Search...");
 
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -214,18 +213,12 @@ public class View extends JFrame implements Observer {
 
 		JPanel suggestionListPanel = new JPanel();
 		suggestionListPanel.setLayout(new BoxLayout(suggestionListPanel, BoxLayout.Y_AXIS));
-
-		//suggestionLabels = new ArrayList<JLabel>();
 		listModel = new DefaultListModel<>();
 		ArrayList<JButton> actionsList = new ArrayList<>();
 
 		for(int i=0; i<10; i++) {
 			String iStr = Integer.toString(i+1);
-			//JLabel suggestion = new JLabel();
 			listModel.addElement("");
-			//suggestionLabels.add(suggestion);
-			//suggestionLabels.get(i).setAlignmentX(Component.LEFT_ALIGNMENT);
-			//suggestionListPanel.add(suggestionLabels.get(i));
 
 		}
 
@@ -245,25 +238,15 @@ public class View extends JFrame implements Observer {
 
 		allResults = new JLabel();
 		selectedResults = new JLabel();
-		//JLabel resulsToGoLabel = new JLabel("Result :");
-		//resultsToGo = new JComboBox();
-		//resultsToGo.addItem("No results");
 		JButton previous = new JButton("Previous");
 		previous.addActionListener(Controller);
 		JButton next = new JButton("Next");
 		next.addActionListener(Controller);
-		//go = new JButton("Go");
-		//go.addActionListener(Controller);
 
 		navigation.add(allResults);
 		navigation.add(selectedResults);
 		navigation.add(previous);
 		navigation.add(next);
-		//navigation.add(resulsToGoLabel);
-		//navigation.add(resultsToGo);
-		//navigation.add(go);
-
-
 		contentPane.add(navigation, BorderLayout.SOUTH);
 		setSize(600,600);
 
@@ -356,13 +339,9 @@ public class View extends JFrame implements Observer {
 			}
 
 
-			//resultExpanded = new JPanel();
-			//resultExpanded.setLayout(new BoxLayout(resultExpanded, BoxLayout.Y_AXIS));
 
 			expandedID = new JLabel(title);
 
-
-			//expandedDate = new JLabel(date);
 
 			resultExpanded.removeAll();
 			resultExpanded.add(expandedID);
@@ -371,8 +350,6 @@ public class View extends JFrame implements Observer {
 
 			scroll.setSize(new Dimension(600, 600));
 
-			//getContentPane().remove(resultExpanded);
-			//getContentPane().add(resultExpanded, BorderLayout.EAST);
 			invalidate();
 			validate();
 			repaint();
@@ -388,21 +365,15 @@ public class View extends JFrame implements Observer {
 			finish = 10;
 		}
 
-		//resultsToGo.removeAllItems();
-		//listModel.removeAllElements();
-
 
 
 		for(int i=0; i<finish; i++) {
 			QueryResult result = suggestionsList.getResults().get(start+i);
-			//suggestionLabels.get(i).setText("<html><font color='blue'>" + result.getmDocName() + "</font><br>" + getMentions(result) + "</html>");
 			listModel.set(i, "<html><font color='blue'>" + result.getmDocName() + "</font><br>" + getMentions(result) + "</html>");
-			//resultsToGo.addItem(result.getmDocName());
 		}
 		
 		for(int i=finish; i<10; i++) {
 			listModel.set(i, "");
-			//suggestionLabels.get(i).setText("");
 		}
 
 		if(suggestionsList.size() == 0) {
